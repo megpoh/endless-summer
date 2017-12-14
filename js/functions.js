9f8d49ea5d00a54b61024ae88aca0171f91c4157
megpoh <megpoh317@yahoo.com>
2017-12-14 12:12:11 -0600

    Assignment 7 - OFFICIALLY done
    Finished Endless Summer Website yuh

--- js/functions.js
+++ js/functions.js
@@ -22,5 +22,23 @@ $(document).ready(function(){
       google.maps.event.addDomListener(window, 'load', initialize);
 	
 	
+	$('.smooth-scroll').on('click', 'a', function(e) {
+			// prevent default anchor click behavior
+   			e.preventDefault();
+		
+		   // store hash
+		   var hash = this.hash;
+			
+			$('html, body').animate({
+			   scrollTop: $(this.hash).offset().top
+			 }, 1000, function(){
+		
+			   // when done, add hash to url
+			   // (default click behaviour)
+			   window.location.hash = hash;
+			 });
+		
+		});
+	
 })
 
