5a871e422b74a22130aacf1081fd84146b37d2aa
megpoh <megpoh317@yahoo.com>
2017-12-14 11:33:17 -0600

    12/14 - Assignment 7
    Finished website WOO

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,26 @@
 // JavaScript Document
 
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+	
+	  function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+          center: new google.maps.LatLng(-37.824787, 174.803721),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+	
+	
+})
 
